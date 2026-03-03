900dd4104a3c2a674b4976002360797cf4ad9ae6
Xyleee. <jameskesbacosa24@gmail.com>
2026-03-03 10:59:23 +0800

    configure github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Xyleee.github.io',
+  base: '/magazineportfolio-astro/', // important!
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
